87073bea10f358bd9b6927fcfad6f6241d97be18
mkozlowski98 <maciekkozlowski98@gmail.com>
2019-11-24 23:43:36 +0100

    drawing state

--- sokoban3.hs
+++ sokoban3.hs
@@ -11,6 +11,15 @@ box  = (cross 0.3 0.3) & (cross (-0.3) 0.3) & colored brown square
 cross :: Double -> Double -> Picture
 cross x y = colored black (polyline([(-x, -y), (x, y)]))
 
+player1 :: Picture
+player1 = (colored yellow (solidPolygon([(0, -0.4), ((0.4*(sqrt 3)/2), 0.4/2), ((-0.4*(sqrt 3)/2), 0.4/2)])))
+
+player2 :: Direction -> Picture
+player2 R = rotated (pi/2) player1
+player2 L = rotated (3*pi/2) player1
+player2 U = rotated pi player1
+player2 D = player1
+
 data Tile = Wall | Ground | Storage | Box | Blank deriving (Eq)
 
 drawTile :: Tile -> Picture
@@ -19,7 +28,34 @@ drawTile Ground  = ground
 drawTile Storage = storage
 drawTile Box     = box
 drawTile Blank   = blank
-  
+
+data Coord = C Integer Integer deriving (Eq)
+
+initialCoord :: Coord
+initialCoord = C 3 3
+
+atCoord :: Coord -> Picture -> Picture
+atCoord (C x y) pic = translated (fromIntegral x) (fromIntegral y) pic
+
+checkIfAllowed :: Coord -> Bool
+checkIfAllowed coord
+  | maze coord == Storage || maze coord == Ground = True
+  | otherwise                                     = False
+
+adjacentCoord :: Direction -> Coord -> Coord
+adjacentCoord R (C x y) = C (x+1) y
+adjacentCoord L (C x y) = C (x-1) y
+adjacentCoord U (C x y) = C x (y+1)
+adjacentCoord D (C x y) = C x (y-1)
+
+checkCoord :: Coord -> Direction -> Bool
+checkCoord coord direction
+  | checkIfAllowed (adjacentCoord direction coord) == True                                                = True
+  | maze (adjacentCoord direction coord) == Box && checkIfAllowed (adjacentCoord direction coord) == True = True
+  | otherwise                                                                                             = False
+
+type Maze = Coord -> Tile
+
 maze :: Maze
 maze (C x y)
   | abs x > 4  || abs y > 4  = Blank
@@ -32,16 +68,18 @@ maze (C x y)
 boxes :: [Coord]
 boxes = [(C x y) | x <- [0,1], y <- [0, 1]]
 
-pictureOfMaze :: Picture
-pictureOfMaze = pictures([translated (fromIntegral x) (fromIntegral y) (drawTile (addBoxes boxes (removeBoxes maze) (C x y))) | x<-[-10..10], y<-[-10..10]])
+pictureOfMaze :: Maze -> Picture
+pictureOfMaze func = pictures([translated (fromIntegral x) (fromIntegral y) (drawTile (func (C x y))) | x<-[-10..10], y<-[-10..10]])
 
 data Direction = R | U | L | D
 
-data Coord = C Integer Integer deriving (Eq)
+initialDirection :: Direction
+initialDirection = D
 
-data State = Coord Direction [Coord]
+data State = S Coord Direction [Coord]
 
-type Maze = Coord -> Tile
+initialState :: State
+initialState = S initialCoord initialDirection (initialBoxes maze)
 
 initialBoxes :: Maze -> [Coord]
 initialBoxes func = [(C x y) | x <- [-10..10], y <- [-10..10], (func (C x y)) == Box]
@@ -56,5 +94,19 @@ addBoxes boxes func = func'
   where func' coord | (elem coord boxes) == True = Box
         func' coord = func coord
 
+draw :: State -> Picture
+draw (S coord direction boxes) = (atCoord coord (player2 direction)) & (pictureOfMaze maze)
+
+adjacentState :: State -> State
+adjacentState (S coord direction boxes)
+  | maze adjacentCoord direction coord == Box = (S (adjacentCoord direction coord) direction )
+
+handleEvent :: Event -> State -> State
+handleEvent (KeyPress key) (S coord direction boxes)
+  | key == "Right" && checkCoord coord R = 
+  | key == "Left" && checkCoord coord L  =
+  | key == "Up" && checkCoord coord U    =
+  | key == "Down" && checkCoord coord D  = 
+
 main :: IO ()
-main = drawingOf pictureOfMaze
+main = drawingOf (draw initialState)
