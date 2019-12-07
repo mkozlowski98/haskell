edfe2f77f09ccd49e6cc2e14f171b3d24f0d5405
mkozlowski98 <maciekkozlowski98@gmail.com>
2019-12-07 05:00:59 +0100

    stage 5 uncompleted

--- sokoban4.hs
+++ sokoban4.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE OverloadedStrings #-}
 import CodeWorld
+import Data.Maybe
 
 elemList :: Eq a => a -> [a] -> Bool
 elemList _ [] = False
@@ -51,6 +53,12 @@ box  = (cross 0.3 0.3) & (cross (-0.3) 0.3) & colored brown square
 player1 :: Picture
 player1 = (colored yellow (solidPolygon([(0, -0.4), ((0.4*(sqrt 3)/2), 0.4/2), ((-0.4*(sqrt 3)/2), 0.4/2)])))
 
+player2 :: Direction -> Picture
+player2 R = rotated (pi/2) player1
+player2 L = rotated (3*pi/2) player1
+player2 U = rotated pi player1
+player2 D = player1
+
 cross :: Double -> Double -> Picture
 cross x y = colored black (polyline([(-x, -y), (x, y)]))
 
@@ -68,7 +76,142 @@ data Coord = C Integer Integer deriving (Eq)
 atCoord :: Coord -> Picture -> Picture
 atCoord (C x y) pic = translated (fromIntegral x) (fromIntegral y) pic
 
+checkIfFree :: Coord -> (Coord -> Tile) -> Bool
+checkIfFree coord myMaze
+  | myMaze coord == Storage || myMaze coord == Ground = True
+  | otherwise                                         = False
+
+checkIfBoxAllowed :: Direction -> Coord -> (Coord -> Tile) -> Bool
+checkIfBoxAllowed direction coord myMaze
+  | myMaze coord == Box && checkIfFree (adjacentCoord direction coord) myMaze = True
+  | otherwise                                                                 = False
+
+adjacentCoord :: Direction -> Coord -> Coord
+adjacentCoord R (C x y) = C (x+1) y
+adjacentCoord L (C x y) = C (x-1) y
+adjacentCoord U (C x y) = C x (y+1)
+adjacentCoord D (C x y) = C x (y-1)
+
+adjacentCoordIfAllowed :: Coord -> Direction -> (Coord -> Tile) -> Coord
+adjacentCoordIfAllowed coord direction myMaze =
+  if checkIfFree (adjacentCoord direction coord) myMaze || checkIfBoxAllowed direction (adjacentCoord direction coord) myMaze then adjacentCoord direction coord
+  else coord
+
 data Maze = Maze Coord (Coord -> Tile)
+
+data Direction = R | U | L | D
+
+initialDirection :: Direction
+initialDirection = D
+
+data State = S Coord Direction [Coord]
+instance Eq State where
+  S coord direction boxes == S coord' direction' boxes' = coord == coord' && boxes == boxes'
+
+initialState :: Maze -> State
+initialState maze@(Maze coord drawMaze) = S coord initialDirection (initialBoxes maze)
+
+initialBoxes :: Maze -> [Coord]
+initialBoxes (Maze coord func) = filterList (\x -> if (func x == Box) then True else False) (bfsGraph (coord : []) (neighbours coord) neighbours)
+                                 where neighbours (C x y) = filterList checkIfWall [(C (x-1) y), (C (x+1) y), (C x (y-1)), (C x (y+1))]
+                                       checkIfWall coord = func coord /= Wall
+
+removeBoxes :: (Coord -> Tile) -> (Coord -> Tile)
+removeBoxes func = func'
+  where func' coord | (func coord) == Box = Ground
+        func' coord = (func coord)
+        
+addBoxes :: [Coord] -> (Coord -> Tile) -> (Coord -> Tile)
+addBoxes boxes func = func'
+  where func' coord | (elem coord boxes) == True = Box
+        func' coord = func coord
+
+adjacentBoxes :: [Coord] -> Coord -> Direction -> [Coord]
+adjacentBoxes boxes coord direction = map (\x -> if (x == coord) then (adjacentCoord direction coord) else x) boxes
+
+draw :: State -> Picture
+draw (S coord direction boxes) =
+  if isWinning (S coord direction boxes) == True then (lettering "Poziom ukończony, liczba ruchów: ")
+  else (atCoord coord (player2 direction)) & (pictureOfMaze (addBoxes boxes (removeBoxes maze)))
+
+keyToDirection :: Event -> Maybe Direction
+keyToDirection (KeyPress key)
+  | key == "Right" = Just R
+  | key == "Left"  = Just L
+  | key == "Up"    = Just U
+  | key == "Down"  = Just D
+keyToDirection _ = Nothing
+
+handleEvent :: Event -> State -> State
+handleEvent event state@(S coord direction boxes) =
+  case keyToDirection event of
+    Nothing -> state
+    Just x -> if isWinning state then state
+              else (S newCoord newDirection (adjacentBoxes boxes newCoord newDirection))
+              where
+                 newCoord = adjacentCoordIfAllowed coord newDirection actMaze
+                 newDirection = x
+                 actMaze = addBoxes boxes (removeBoxes maze)
+
+checkIfStorage :: Coord -> Bool
+checkIfStorage coord = maze coord == Storage
+
+isWinning :: State -> Bool
+isWinning (S coord direction boxes) = allList checkIfStorage boxes
+
+data Activity world = Activity world (Event -> world -> world) (world -> Picture)
+
+resettable :: Activity s -> Activity s
+resettable (Activity state0 handle draw) = Activity state0 handle' draw
+    where handle' (KeyPress key) _ | key == "Esc" = state0
+          handle' e s = handle e s
+
+startScreen :: Picture
+startScreen = scaled 3 3 (lettering "Sokoban!") & (translated 0 (-5) etap4)
+
+data SSState world = StartScreen | Running world
+
+withStartScreen :: Activity s -> Activity (SSState s)
+withStartScreen (Activity state0 handle draw) = Activity state0' handle' draw'
+  where
+    state0' = StartScreen
+
+    handle' (KeyPress key) StartScreen
+         | key == " "                  = Running state0
+    handle' _              StartScreen = StartScreen
+    handle' e              (Running s) = Running (handle e s)
+
+    draw' StartScreen = startScreen
+    draw' (Running s) = draw s
+
+data WithUndo a = WithUndo a [a]
+
+withUndo :: Eq a => Activity a -> Activity (WithUndo a)
+withUndo (Activity state0 handle draw) = Activity state0' handle' draw' where
+    state0' = WithUndo state0 []
+    handle' (KeyPress key) (WithUndo s stack) | key == "U"
+      = case stack of s':stack' -> WithUndo s' stack'
+                      []        -> WithUndo s []
+    handle' e              (WithUndo s stack)
+       | s' == s = WithUndo s stack
+       | otherwise = WithUndo (handle e s) (s:stack)
+      where s' = handle e s
+    draw' (WithUndo s _) = draw s
+
+getNumberOfMoves :: WithUndo a -> Integer
+getNumberOfMoves (WithUndo x xs) = listLength xs
+
+simple :: Maze -> Activity State
+simple maze = Activity (initialState maze) handleEvent draw
+
+runActivity :: Activity s -> IO ()
+runActivity (Activity state0 handle draw0) = activityOf state0 handle draw0
+
+etap5 :: IO ()
+etap5 = (runActivity (withStartScreen (resettable (withUndo (simple mazeGood1)))))
+
+main :: IO ()
+main = etap5
 
 bfsGraph :: Eq a => [a] -> [a] -> (a -> [a]) -> [a]
 bfsGraph seen [] _ = seen
@@ -130,16 +273,16 @@ pictureOfBools xs = translated (-fromIntegral k / 2) (fromIntegral k) (go 0 xs)
 etap4 :: Picture
 etap4 = pictureOfBools (appendList (mapList checkMaze mazes) (mapList checkMaze badMazes))
 
-main :: IO()
-main = drawingOf etap4
+maze :: Coord -> Tile
+maze = drawMazeGood1
 
 mazes :: [Maze]
 mazes = [mazeGood1, mazeGood2]
 badMazes :: [Maze]
 badMazes = [mazeBad1, mazeBad2]
 
-pictureOfMaze :: Maze -> Picture
-pictureOfMaze (Maze coord draw) = (atCoord coord player1) & pictures([translated (fromIntegral x) (fromIntegral y) (drawTile (draw (C x y))) | x<-[-10..10], y<-[-10..10]])
+pictureOfMaze :: (Coord -> Tile) -> Picture
+pictureOfMaze draw = pictures([translated (fromIntegral x) (fromIntegral y) (drawTile (draw (C x y))) | x<-[-10..10], y<-[-10..10]])
 
 mazeGood1 :: Maze
 mazeGood1 = Maze (C 0 3) drawMazeGood1
