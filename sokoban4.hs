c8747caadf5484ddf515b39ef7b0a33125da9738
mkozlowski98 <maciekkozlowski98@gmail.com>
2019-12-07 03:29:00 +0100

    stage 4 + badMaze

--- sokoban4.hs
+++ sokoban4.hs
@@ -41,21 +41,6 @@ foldList :: (a -> b -> b) -> b -> [a] -> b
 foldList f y [] = y
 foldList f y (x:xs) = f x (foldList f y xs)
 
-bfsGraph :: Eq a => [a] -> [a] -> (a -> [a]) -> [a]
-bfsGraph seen [] _ = seen
-bfsGraph seen queue neighbours = if elemList first seen == True then bfsGraph seen (tail queue) neighbours
-                                 else bfsGraph (first : seen) (appendList (tail queue) (neighbours first)) neighbours
-                                 where first = nth queue 1
-
-isGraphClosed :: Eq a => a -> (a -> [a]) -> (a -> Bool) -> Bool
-isGraphClosed initial neighbours isOk = allList isOk (bfsGraph (initial : []) (neighbours initial) neighbours)
-
-reachable :: Eq a => a -> a -> (a -> [a]) -> Bool
-reachable v initial neighbours = elemList v (bfsGraph (initial : []) (neighbours initial) neighbours)
-
-allReachable :: Eq a => [a] -> a -> (a -> [a]) -> Bool
-allReachable vs initial neighbours = andList (mapList isReachable vs) where isReachable x = reachable x initial neighbours
-
 square, wall, ground, storage, box :: Picture
 square = solidRectangle 1 1
 wall = colored black square
@@ -84,10 +69,74 @@ atCoord :: Coord -> Picture -> Picture
 atCoord (C x y) pic = translated (fromIntegral x) (fromIntegral y) pic
 
 data Maze = Maze Coord (Coord -> Tile)
+
+bfsGraph :: Eq a => [a] -> [a] -> (a -> [a]) -> [a]
+bfsGraph seen [] _ = seen
+bfsGraph seen queue neighbours = if elemList first seen == True then bfsGraph seen (tail queue) neighbours
+                                 else bfsGraph (first : seen) (appendList (tail queue) (neighbours first)) neighbours
+                                 where first = nth queue 1
+
+bfsWithCheck :: Eq a => [a] -> [a] -> (a -> [a]) -> (a -> Bool) -> Bool
+bfsWithCheck seen [] _ _ = True
+bfsWithCheck seen queue neighbours isOk = if isOk first == False then False
+                                          else
+                                            if elemList first seen == True then bfsWithCheck seen (tail queue) neighbours isOk
+                                            else bfsWithCheck (first : seen) (appendList (tail queue) (neighbours first)) neighbours isOk
+                                          where first = nth queue 1
+
+isGraphClosed :: Eq a => a -> (a -> [a]) -> (a -> Bool) -> Bool
+isGraphClosed initial neighbours isOk = bfsWithCheck (initial : []) (neighbours initial) neighbours isOk
+
+reachable :: Eq a => a -> a -> (a -> [a]) -> Bool
+reachable v initial neighbours = elemList v (bfsGraph (initial : []) (neighbours initial) neighbours)
+
+allReachable :: Eq a => [a] -> a -> (a -> [a]) -> Bool
+allReachable vs initial neighbours = andList (mapList isReachable vs) where isReachable x = reachable x initial neighbours
+
+isClosed :: Maze -> Bool
+isClosed (Maze coord drawMaze) = if drawMaze coord == Storage || drawMaze coord == Ground then isGraphClosed coord neighbours checkIfBlank
+                                 else False
+                                 where neighbours (C x y) = filterList checkIfWall [(C (x-1) y), (C (x+1) y), (C x (y-1)), (C x (y+1))]
+                                       checkIfWall coord = drawMaze coord /= Wall
+                                       checkIfBlank coord = drawMaze coord /= Blank
+  
+isSane :: Maze -> Bool
+isSane (Maze coord drawMaze) = listLength (filterList checkIfStorage reachableInMaze) >= listLength (filterList checkIfBox reachableInMaze)
+                               where reachableInMaze = bfsGraph (coord : []) (neighbours coord) neighbours
+                                     neighbours (C x y) = filterList checkIfWall [(C (x-1) y), (C (x+1) y), (C x (y-1)), (C x (y+1))]
+                                     checkIfWall coord = drawMaze coord /= Wall
+                                     checkIfStorage coord = drawMaze coord == Storage
+                                     checkIfBox coord = drawMaze coord == Box
+
+checkMaze :: Maze -> Bool
+checkMaze maze = isClosed maze && isSane maze
+
+pictureOfBools :: [Bool] -> Picture
+pictureOfBools xs = translated (-fromIntegral k / 2) (fromIntegral k) (go 0 xs)
+  where n = length xs
+        k = findK 0 -- k is the integer square of n
+        findK i | i * i >= n = i
+                | otherwise  = findK (i+1)
+        go _ [] = blank
+        go i (b:bs) =
+          translated (fromIntegral (i `mod` k))
+                     (-fromIntegral (i `div` k))
+                     (pictureOfBool b)
+          & go (i+1) bs
+
+        pictureOfBool True =  colored green (solidCircle 0.4)
+        pictureOfBool False = colored red   (solidCircle 0.4)
+
+etap4 :: Picture
+etap4 = pictureOfBools (appendList (mapList checkMaze mazes) (mapList checkMaze badMazes))
+
+main :: IO()
+main = drawingOf etap4
+
 mazes :: [Maze]
-mazes = [mazeGood1]
+mazes = [mazeGood1, mazeGood2]
 badMazes :: [Maze]
-badMazes = [mazeBad1]
+badMazes = [mazeBad1, mazeBad2]
 
 pictureOfMaze :: Maze -> Picture
 pictureOfMaze (Maze coord draw) = (atCoord coord player1) & pictures([translated (fromIntegral x) (fromIntegral y) (drawTile (draw (C x y))) | x<-[-10..10], y<-[-10..10]])
@@ -103,10 +152,21 @@ drawMazeGood1 (C x y)
   | abs x == 3 && (y >= -1 && y <= 2)          = Wall
   | abs x == 2 && (y == 3 || y == -2)          = Wall
   | x == 0 && abs y == 1                       = Wall
-  | (x >= -1 && x <= 1) && y == 2              = Box
+  | (x >= -1 && x <= 0) && y == 2              = Box
   | x == -1 && y == -1                         = Box
   | x == 1 && ((y >= 1 && y <= 3) || y == -2)  = Storage
   | otherwise                                  = Ground
+
+mazeGood2 :: Maze
+mazeGood2 = Maze (C 3 3) drawMazeGood2
+drawMazeGood2 :: Coord -> Tile
+drawMazeGood2 (C x y)
+  | abs x > 4  || abs y > 4  = Blank
+  | abs x == 4 || abs y == 4 = Wall
+  | x ==  2 && y <= 0        = Wall
+  | x ==  3 && y <= 0        = Storage
+  | x >= -2 && y == 0        = Box
+  | otherwise                = Ground
 
 mazeBad1 :: Maze
 mazeBad1 = Maze (C (-1) (-2)) drawMazeBad1
@@ -121,5 +181,15 @@ drawMazeBad1 (C x y)
   | x == 2 && (y == 2 || y == -1)           = Storage
   | otherwise                               = Ground
 
-main :: IO ()
-main = drawingOf (pictureOfMaze mazeBad1)
+mazeBad2 :: Maze
+mazeBad2 = Maze (C 0 0) drawMazeBad2
+drawMazeBad2 :: Coord -> Tile
+drawMazeBad2 (C x y)
+  | abs x > 4 || abs y > 4   = Blank
+  | x == 4 && y == 0         = Blank
+  | abs x == 4 || abs y == 4 = Wall
+  | x == -1 && y < 2         = Wall
+  | y == 1 && x < -1         = Wall
+  | x == -3                  = Storage
+  | x == 2 && y < 0          = Box
+  | otherwise                = Ground
